ed12afa609db0e020114b9562144d1c8453271d8
Rangha-Vardhan-1552 <rangavardhang1908@gmail.com>
2024-01-19 23:55:26 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Vardhan Estate</h1>
+      <p className='mb-4 text-slate-700'>Vardhan Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. By this project, Iam providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>
+       The main motto of this project is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>Based on level of experience and knowledge in the real estate industry,I committed to providing the highest level of service to users with better user interfaces.</p>
     </div>
   )
 }
